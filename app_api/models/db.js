4d2dade35e668321d639e84870e36689d91e4085
zrlsena <96860750+zrlsena@users.noreply.github.com>
2022-11-22 19:16:00 +0300

    Update db.js

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 require("./mekansema");
 //var dbURI = "mongodb://localhost/mekanbul";
-var dbURI ="mongodb+srv://sena:1234@mekanbul.1trzw75.mongodb.net/?retryWrites=true&w=majority";
+var dbURI ="mongodb+srv://sena:1234@mekanbul.1trzw75.mongodb.net/mekanbul?retryWrites=true&w=majority";
 mongoose.connect(dbURI);
 
 function kapat(msg, callback) {
